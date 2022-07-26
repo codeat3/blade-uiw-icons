868888667ae59e7cecbaf7912ab4fe3a0b18e0a8
swapnilsarwe <swapnilsarwe@users.noreply.github.com>
2022-07-26 05:25:04 +0000

    Fix styling

--- src/BladeUiwIconsServiceProvider.php
+++ src/BladeUiwIconsServiceProvider.php
@@ -17,24 +17,24 @@ final class BladeUiwIconsServiceProvider extends ServiceProvider
         $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
             $config = $container->make('config')->get('blade-uiw-icons', []);
 
-            $factory->add('uiw-icons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
+            $factory->add('uiw-icons', array_merge(['path' => __DIR__ . '/../resources/svg'], $config));
         });
     }
 
     private function registerConfig(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/blade-uiw-icons.php', 'blade-uiw-icons');
+        $this->mergeConfigFrom(__DIR__ . '/../config/blade-uiw-icons.php', 'blade-uiw-icons');
     }
 
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../resources/svg' => public_path('vendor/blade-uiw-icons'),
+                __DIR__ . '/../resources/svg' => public_path('vendor/blade-uiw-icons'),
             ], 'blade-uiw-icons');
 
             $this->publishes([
-                __DIR__.'/../config/blade-uiw-icons.php' => $this->app->configPath('blade-uiw-icons.php'),
+                __DIR__ . '/../config/blade-uiw-icons.php' => $this->app->configPath('blade-uiw-icons.php'),
             ], 'blade-uiw-icons-config');
         }
     }
